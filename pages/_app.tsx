c5dadd9be5a749d531d53409b01fc6b7a94c48bb
fushuqian <fushuqian@users.noreply.github.com>
2026-05-13 23:47:03 +0800

    Add favicon (pixel logo)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
 import '@/styles/globals.css';
+import Head from 'next/head';
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.png" type="image/png" />
+        <link rel="shortcut icon" href="/favicon.png" type="image/png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
